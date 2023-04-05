35b0f5af71b524935d5812c7100b1d021ed8c1af
nii_okantey <nunoogideon19@gmail.com>
2023-04-05 13:44:22 +0000

    few updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
 
-      </div>
-    );
-  }
 }
 
 export default App;
